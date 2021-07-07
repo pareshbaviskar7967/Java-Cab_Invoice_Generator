b71dba5c9a737af6c279d64cfeace437afeb347e
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-07-07 22:58:07 +0530

    Welcome to the cab invoice generator

---
+++ src/main/java/CabService.java
@@ -0,0 +1,15 @@
+public class CabService {
+    public double calculateFare(final double distance, final int minute) {
+        double totalfare = 0;
+
+        if (distance == 0.0 && minute == 0) {
+            return totalfare = 0;
+        }
+
+        if (distance < 1.0 && minute <= 5) {
+            return totalfare = 5;
+        }
+        totalfare = (distance * 10) + minute;
+        return totalfare;
+    }
+}
